7ea988194e13e7f5dba69905aab42c45bf33a3fa
mahendrarathore468-creator <mahendrarathore468@gmail.com>
2026-06-06 07:45:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
 TOKEN = "8906344428:AAGSdHseDYf4WcXWE8LJGnQW_ALeT6_hY60"
 
@@ -12,6 +12,8 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Instagram link bhejo.")
 
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.TEXT, handle))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
